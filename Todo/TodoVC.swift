5a5dd5666334bdf4ea02f193ef02ae2f352bcfc1
Eugene Golovanov <eugene.golovanov@hart.com>
2016-07-16 03:42:00 -0700

    table view added

--- Todo/TodoVC.swift
+++ Todo/TodoVC.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class TodoVC: UIViewController {
+class TodoVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
-    var todos = [Todo]()
     
     //--------------------------------------------------------------------------------------------------
     //MARK: - Properties
 
+    var todos = [Todo]()
     
-    
+    @IBOutlet weak var todoTableView: UITableView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     //--------------------------------------------------------------------------------------------------
     //MARK: - View Lifecycle
@@ -36,29 +37,21 @@ class TodoVC: UIViewController {
 
         if let token = NSUserDefaults.standardUserDefaults().stringForKey(KEY_TOKEN) {
             print("Token:\n\(token)")
+            spinner.startAnimating()
+            self.getAllTodosResponse()
         } else {
             performSegueWithIdentifier("loginSegue", sender: self)
         }
+        
         
 
     }
     
     
     //--------------------------------------------------------------------------------------------------
-    //MARK: - Actions
-
-    @IBAction func onLogoutButton(sender: UIButton) {
-        
-        deleteToken()
-        performSegueWithIdentifier("loginSegue", sender: self)
-
-        
-    }
+    //MARK: - Responses
     
-    
-    @IBAction func onGetAllTodos(sender: UIButton) {
-        print("\nGetting All Todos")
-        
+    func getAllTodosResponse() {
         ////GET Todos////
         if let token = NSUserDefaults.standardUserDefaults().stringForKey(KEY_TOKEN) {
             API.get(URL_TODOS, attachToken: true, alternateToken: token, completed: { (response) in
@@ -70,28 +63,76 @@ class TodoVC: UIViewController {
                         let allTodos = AllTodos(data: data)
                         self.todos = allTodos.todos
                         print(self.todos)
-                    
+                        
                     }
                     print("------------------------------------------")
                     
                     dispatch_async(dispatch_get_main_queue(),{
-                        let alert = UIAlertController(
-                            title: "Got All TODOS",
-                            message: "I Have All TODOS",
-                            preferredStyle: .Alert)
-                        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-                        self.presentViewController(alert, animated: true, completion: nil)
+//                        let alert = UIAlertController(
+//                            title: "Got All TODOS",
+//                            message: "I Have All TODOS",
+//                            preferredStyle: .Alert)
+//                        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+//                        self.presentViewController(alert, animated: true, completion: nil)
+                        
+                        self.todoTableView.reloadData()
+                        self.spinner.stopAnimating()
                     })
-
+                    
                 }
             })
-
+            
         }
         ////////////////////
-        
-        
-        
     }
+
+    
+    
+    //--------------------------------------------------------------------------------------------------
+    //MARK: - Actions
+
+    @IBAction func onLogoutButton(sender: UIButton) {
+        deleteToken()
+        performSegueWithIdentifier("loginSegue", sender: self)
+    }
+    
+    
+    @IBAction func onGetAllTodos(sender: UIButton) {
+        print("\nGetting All Todos")
+        self.getAllTodosResponse()
+    }
+    
+    @IBAction func onAddTodoAction(sender: UIButton) {
+        self.performSegueWithIdentifier("addTodo", sender: self)
+    }
+    
+    //--------------------------------------------------------------------------------------------------
+    // MARK: - -Table View-
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.todos.count
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ident", forIndexPath: indexPath)
+        
+        let todo = self.todos[indexPath.row]
+        cell.textLabel!.text = todo.description
+        cell.detailTextLabel!.text = todo.completed.description ?? "none"
+        // Configure the cell...
+        
+        return cell
+    }
+
+    
+
 
 }
 
