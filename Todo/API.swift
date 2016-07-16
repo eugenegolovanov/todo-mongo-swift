1f2c0954d0a2788758631f990e5accfb6dd3a63b
Eugene Golovanov <eugene.golovanov@hart.com>
2016-07-15 18:26:28 -0700

    login signup implemented with API

---
+++ Todo/API.swift
@@ -0,0 +1,335 @@
+//
+//  API.swift
+//  Todo
+//
+//  Created by eugene golovanov on 7/14/16.
+//  Copyright © 2016 eugene golovanov. All rights reserved.
+//
+
+import Foundation
+
+enum ResponseType:ErrorType{
+    case Success
+    case ServerError
+    case ValidationError
+    case Unauthorized
+    case PreconditionFail
+    case UndefinedResponse
+}
+
+
+
+public class APIResponse: CustomDebugStringConvertible {
+    public var code:Int
+    public var success:Bool
+    public var rawData:AnyObject?
+    public var data:Dictionary<String,AnyObject>?
+    public var dataArray:Array<Dictionary<String, AnyObject>>?
+    public var multiDataArray:Array<Array<Dictionary<String, AnyObject>>>?
+    public var dataNum:Int?
+    public var dataString:String?
+    public var error:NSError?
+    public let request:NSURLRequest?
+    public let duration:NSTimeInterval
+    
+    init(request:NSURLRequest?, duration:NSTimeInterval, payload:AnyObject?, error:NSError?){
+        self.code = -1
+        self.success = false
+        self.data = nil
+        self.error = nil
+        self.request = request
+        self.duration = duration
+        
+        if(error != nil){
+            self.error = error
+        }
+        else if let obj = payload {
+            self.rawData = obj
+            if let dict = obj as? Dictionary<String,AnyObject>,
+                code = dict["code"] as? Int,
+                success = dict["success"] as? Bool {
+                
+                self.success = success
+                self.code = code
+                
+                if let data = dict["data"] as? Dictionary<String, AnyObject> {
+                    self.data = data
+                } else if let data = dict["data"] as? Array<Dictionary<String, AnyObject>> {
+                    self.dataArray = data
+                } else if let data = dict["data"] as? Array<Array<Dictionary<String, AnyObject>>> {
+                    self.multiDataArray = data
+                } else if let data = dict["data"] as? String {
+                    self.dataString = data
+                } else if let data = dict["data"] as? [Int] {
+                    if data.count > 0{
+                        self.dataNum = data[0]
+                    }
+                }
+            }
+        }
+        
+//        print(self.debugDescription)
+        if self.success == false {
+//            print(self.request!.cURL())
+        }
+    }
+    
+    public var debugDescription: String {
+        var descr = "<APIResponse"
+        
+        if let r = self.request, u = r.URL, m = r.HTTPMethod {
+            descr += " \(m) \(u)"
+        }
+        
+        descr += " success=\(self.success) code=\(self.code)"
+        
+        if let err = self.error {
+            descr += " error=\(err)"
+        }
+        
+//        if let r = self.request, t = r.valueForHTTPHeaderField("Auth") {
+//            descr += " token=\(t)"
+//        }
+        
+        let d = NSString(format: "%.2f", self.duration)
+        return descr + " duration=\(d)s>"
+    }
+}
+
+
+
+
+
+public class API {
+    
+//    static let manager:Manager = {
+//        let serverTrustPolicies: [String: ServerTrustPolicy] = [
+//            "prov-stg.hart.com": .PinCertificates(
+//                certificates: ServerTrustPolicy.certificatesInBundle(),
+//                validateCertificateChain: true,
+//                validateHost: true
+//            )
+//        ]
+//        print("Found certs in bundle: \(ServerTrustPolicy.certificatesInBundle())")
+//        return Manager(serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies))
+//    }()
+    
+    
+    
+    
+    
+    
+    //Get
+    public class func get(url:AnyObject, attachToken:Bool = true, alternateToken:String? = nil, completed: (response:APIResponse) -> Void) {
+        self.createURLRequest(url, method: "GET", attachToken: attachToken, alternateToken: alternateToken, content: nil, completed: completed)
+    }
+    
+    //Post
+    public class func post(url:AnyObject, payload:[String:AnyObject]? = nil, attachToken:Bool = true, alternateToken:String? = nil, completed: (response:APIResponse) -> Void) {
+        self.createURLRequest(url, method: "POST", attachToken: attachToken, alternateToken: alternateToken, content: payload, completed: completed)
+    }
+    
+    //Put
+    public class func put(url:AnyObject, payload:[String:AnyObject]? = nil, attachToken:Bool = true, alternateToken:String? = nil, completed: (response:APIResponse) -> Void) {
+        self.createURLRequest(url, method: "PUT", attachToken: attachToken, alternateToken: alternateToken, content: payload, completed: completed)
+    }
+    
+    //Delete
+    public class func delete(url:AnyObject, attachToken:Bool = true, alternateToken:String? = nil, completed: (response:APIResponse) -> Void) {
+        self.createURLRequest(url, method: "DELETE", attachToken: attachToken, alternateToken: alternateToken, content: nil, completed: completed)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private class func createURLRequest(u:AnyObject, method:String, attachToken:Bool, alternateToken:String? = nil, content:[String:AnyObject]? = nil, completed: (response:APIResponse) -> Void) -> Void {
+        
+        let startTime = NSDate()
+        
+        
+//        let sendResponse: (NSURLRequest?, AnyObject?, NSError?) -> Void = { (request, data, error) -> Void in
+//            dispatch_async(dispatch_get_main_queue(),{
+//                completed(response: APIResponse(request: request, duration: NSDate().timeIntervalSinceDate(startTime), payload: data, error: error))
+//            })
+//        }
+        
+        let url:NSURL
+        
+        if let ur = u as? NSURL {
+            url = ur
+        } else if let ur = u as? String, uu = NSURL(string: ur) {
+            url = uu
+        } else {
+            completed(response: APIResponse(request: nil, duration: 0, payload: nil, error: nil))
+            return
+        }
+        let request = NSMutableURLRequest(URL: url)
+        
+        
+        //If Token
+        if attachToken == true {
+            guard let token = alternateToken else {
+                print("Token Problem")
+                return
+            }
+            print("\nAPI \(method) \(url) with token \(token)")
+            request.addValue(token, forHTTPHeaderField: "Auth")
+        }
+        
+
+        
+        
+        
+
+		request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+		request.HTTPMethod = method
+		
+		let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+		sessionConfig.timeoutIntervalForRequest = 30
+
+        //POST
+        if method == "POST" {
+            if let params = content {
+                
+                do {
+                    request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: NSJSONWritingOptions())
+                } catch let error as NSError {
+                    print("Error in request post: \(error)")
+                    request.HTTPBody = nil
+                } catch {
+                    print("Catch all error: \(error)")
+                }
+            }
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+                
+                guard let httpResponse = response as? NSHTTPURLResponse else{
+                    print("No Response")
+                    return
+                }
+                //OBJ fo response
+                var obj = Dictionary<String,AnyObject>()
+                obj["code"] = httpResponse.statusCode
+                obj["success"] = API.statusSuccessChecker(httpResponse.statusCode)
+                
+                //Token If exists
+                if let token = httpResponse.allHeaderFields["Auth"] {
+                    obj["data"] = token as? String
+                }
+
+                completed(response: APIResponse(request: request, duration: NSDate().timeIntervalSinceDate(startTime), payload: obj, error: error))
+            }
+
+            task.resume()
+
+        }
+        //GET
+        else if method == "GET" {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+                (data:NSData?, response:NSURLResponse?, error:NSError?) in
+                
+                //in case of error
+                if error != nil {
+                    print("shit err")
+                    completed(response: APIResponse(request: request, duration: NSDate().timeIntervalSinceDate(startTime), payload: data, error: error))
+
+                    return
+                } else {
+                    guard let data = data else {print("error getting data"); return}
+                    
+
+                    
+                    do {
+                        guard let httpResponse = response as? NSHTTPURLResponse else{
+                            print("No Response")
+                            return
+                        }
+                        //OBJ fo response
+                        var obj = Dictionary<String,AnyObject>()
+                        obj["code"] = httpResponse.statusCode
+                        obj["success"] = API.statusSuccessChecker(httpResponse.statusCode)
+
+                        
+                        
+                            let json = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+                            print("\nJSON:\(json)")
+                            
+                            //If ARRAY
+                            if let todoArray = json as? [AnyObject] {
+                                obj["data"] = todoArray
+                            }
+                                //If DICT
+                            else {
+                                obj = json as! Dictionary<String, AnyObject>
+                            }
+
+                        
+                        completed(response: APIResponse(request: request, duration: NSDate().timeIntervalSinceDate(startTime), payload: obj, error: error))
+                        
+                    } catch {
+                        print("ERROR:\(error)")
+                    }
+                }
+            
+
+            }
+            task.resume()
+
+        }
+            
+            
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public class func statusSuccessChecker(code:Int) -> Bool {
+        var success = false
+        
+             if code == 200 {
+                print("SUCCESS.........200")
+                success = true
+             }
+        else if code == 201 {
+                print("SUCCESS.........201")
+                success = true
+             }
+        
+        else if code == 500 {
+                print("SERVER ERROR.........500")
+             }
+        
+        else if code == 400 {
+                print("VALIDATION ERROR.........400")
+             }
+        
+        else if code == 401 {
+                print("UNAUTHORIZED ERROR.........401")
+             }
+        
+        else if code == 412 {
+                print("PRECONDITIONAL FAIL ERROR.........412")
+             }
+        
+        else  {
+                print("UNDEFINED RESPONSE")
+        }
+    
+        return success
+    }
+
+
+    
+
+
+}
--- Todo/LoginVC.swift
+++ Todo/LoginVC.swift
@@ -10,26 +10,223 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    //--------------------------------------------------------------------------------------------------
+    //MARK: - Properties
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    //--------------------------------------------------------------------------------------------------
+    //MARK: - View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //--------------------------------------------------------------------------------------------------
+    //MARK: - Actions
+    
+    @IBAction func onSegmentControl(sender: UISegmentedControl) {
+//            print("\(sender.selectedSegmentIndex)")
+        
+        if sender.selectedSegmentIndex == 0 {
+            loginButton.setTitle("Login", forState: .Normal)
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            loginButton.setTitle("Signup", forState: .Normal)
+
+        }
+        
+        
     }
     
+    @IBAction func onLoginButton(sender: UIButton) {
+        
+        let api = "https://egtodo.herokuapp.com"
+//        let api = "https://localhost:3000"
 
-    /*
-    // MARK: - Navigation
+        let todos = "/todos"
+        let login = "/users/login"
+        let signup = "/users"
+        
+        var url = api
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
+        
+        let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6IlUyRnNkR1ZrWDE5YmxrOVN6b1NENHQ2azJxd3Jqc29LMFIydG1GQ0tDNFFKVlN0d0VXeDFQUmZFM2xVRTdJVXMxcTdSc3ZWM0VuNlhKQ0JXYjdxRFhRPT0iLCJpYXQiOjE0Njg2MjYxMTh9.2cf2Np-b28630XRp_WPYFwsqor6_bG4gDyd4anTz5Mg"
+        
+        
+        guard let emailStr    = self.emailField.text else {print("No Email"); return}
+        guard let passwordStr = self.passwordField.text else {print("No Password"); return}
+        print("EMAIL:\(emailStr)   PASSWORD:\(passwordStr)")
+        let paramsDictionary = ["email":emailStr, "password":passwordStr]
+        
+        
+        
+        
+        if sender.titleLabel?.text == "Login" {
+            print("Perform LOGIN")
+            url = api + login
+            print("--URL:--  \(url)")
+            
+            
+            //-LOGIN-//
+            API.post(url, payload: paramsDictionary, attachToken: false, alternateToken: nil, completed: { (response) in
+                
+                print("----------------------------------------------------------------------------------")
+                if let token = response.dataString {
+                    print("token: \(token)")
+                }
+                print("----------------------------------------------------------------------------------")
+
+            })
+            //-END OF LOGIN-//
+        
+            
+            
+            
+            
+            
+            
+            
+        }
+        else if sender.titleLabel?.text == "Signup"{
+            print("Perform Signup")
+            url = api + signup
+            print("--URL:--  \(url)")
+            
+            
+            
+            
+            //-SIGNUP-//
+            API.post(url, payload: paramsDictionary, attachToken: false, alternateToken: nil, completed: { (response) in
+                
+                print("----------------------------------------------------------------------------------")
+                print(response)
+                print("----------------------------------------------------------------------------------")
+                
+            })
+            //-END OF SIGNUP-//
+            
+            
+            
+        }
     }
-    */
+    
+    
+    
+    
+    
+    
+    
+    //--------------------------------------------------------------------------------------------------
+    //MARK: - API
+    
+    
+    
+    
+    
+    func postLoginRequest(params:Dictionary<String, String>, completion: (data: NSData?, response:NSURLResponse?, error:NSError?) -> Void) {
+        let urlPath: String = "https://egtodo.herokuapp.com/users/login"
+        let url = NSURL(string: urlPath)
+        let request = NSMutableURLRequest(URL: url!);
+        request.HTTPMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        let session = NSURLSession.sharedSession()
+        
+        // Verify downloading data is allowed
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: NSJSONWritingOptions())
+        } catch let error as NSError {
+            print("Error in request post: \(error)")
+            request.HTTPBody = nil
+        } catch {
+            print("Catch all error: \(error)")
+        }
+        
+        
+        
+        // Post the data
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            completion(data: data, response: response, error: error)
+        }
+        
+        task.resume()
+        
+    }
+
+    
+    
+    
+    
+    func getAllTodos(token token:String, completion: (data: NSData?, response:NSURLResponse?, error:NSError?) -> Void) {
+        
+        let urlPath: String = "https://egtodo.herokuapp.com/todos"
+        let url = NSURL(string: urlPath)
+        let request = NSMutableURLRequest(URL: url!);
+        //Token
+        request.addValue(token, forHTTPHeaderField: "Auth")
+        request.HTTPMethod = "GET"
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            (data:NSData?, response:NSURLResponse?, error:NSError?) in
+            
+            //in case of error
+            if error != nil {
+                print("shit err")
+                completion(data: data, response: response, error: error)
+
+                return
+            } else {
+                guard let data = data else {print("error getting data"); return}
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+                    print("JSON:\(json)")
+                    
+                    completion(data: data, response: response, error: error)
+                    
+                    
+//                    //If NOT Array
+//                    if let desc = json["description"] as? String {
+//                        print("JSON is NOT Array")
+//                        print("DESCRIPT: \(desc)")
+//                    }
+//                    
+//                    //If ARRAY
+//                    if let todoArray = json as? [AnyObject] {
+//                        print("JSON is ARRAY")
+//                        
+//                        for todo in todoArray  {
+//                            if let desc = todo["description"] as? String,
+//                                let id = todo["id"] as? Int {
+//                                print("-----------------------------------")
+//                                print("id:\(id)  DESCRIPT: \(desc)")
+//                            }
+//                        }
+//                    }
+                    
+                    
+                    
+                    
+                } catch {
+                    print("ERROR:\(error)")
+                }
+            }
+        }
+        task.resume();
+    }
+
+    
+    
+    
+    
+ 
+
+
 
 }
